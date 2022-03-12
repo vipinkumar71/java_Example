fee749a433a311916dc98be703068501ce1e0d54
Vipin Kumar <vipinthakur3881@gmail.com>
2022-03-12 23:58:53 +0530

    just reformat the code

--- src/Banking_application.java
+++ src/Banking_application.java
@@ -11,14 +11,16 @@ public class Banking_application {
         int operation = sc.nextInt();
         if(operation== 1){
             System.out.println("Exiting from system");
-        }else if (operation==2){
+        }
+        else if (operation==2){
             System.out.println("How much money do you have?");
             int balance = sc.nextInt(); // 500
             System.out.println("How much money you want to transfer");
             int transfer = sc.nextInt();// 100
             if(balance<transfer){
                 System.out.println("You can not do this operation, because your balance is not enough");
-            }else if(balance>=transfer){
+            }
+            else if(balance>=transfer){
                 System.out.println("You transferred " + transfer);
                 System.out.println("Your current balance is: " +(balance-transfer));
             }
@@ -34,7 +36,8 @@ public class Banking_application {
             int money = sc.nextInt();
             if(balance<money){
                 System.out.println("Your balance is not enough to do this operation");
-            }else if (balance > money){
+            }
+            else if (balance > money){
                 System.out.println("You got your money " +money);
                 System.out.println("Your current balance is: " +(balance - money));
             }
@@ -43,7 +46,8 @@ public class Banking_application {
             }
 
 
-        }else{
+        }
+        else{
             System.out.println("Invalid operation");
         }
     }
