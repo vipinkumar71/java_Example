c6e070ac1d2187644682544ac51627e0bfa87d8f
Vipin Kumar <vipinthakur3881@gmail.com>
2022-03-13 02:42:11 +0530

    While loop in java

---
+++ src/WhileLoop.java
@@ -0,0 +1,9 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int a =0;
+        while(a<10) {
+            a++;
+            System.out.println(a);
+        }
+    }
+}
